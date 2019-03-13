210f80fcdf22747eb602987ea6773a87b976cce6
lucas.racoci <racoci.0@gmail.com>
2019-03-13 17:57:44 -0300

    Building but without result from DataBases, todo: Test just retrofit

---
+++ app/src/main/java/com/lracoci/weatherforecast/WeatherForecastApplication.kt
@@ -0,0 +1,4 @@
+package com.lracoci.weatherforecast
+
+class Application {
+}
---
+++ app/src/main/java/com/lracoci/weatherforecast/ui/LifecycleBoundLocationManager.kt
@@ -0,0 +1,5 @@
+package com.lracoci.weatherforecast.ui
+
+class LifecycleBoundLocationManager(mainActivity: MainActivity, fusedLocationProviderClient: FusedLocationProviderClient, locationCallback: LocationCallback) {
+
+}
